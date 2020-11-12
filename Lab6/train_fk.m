5b8ce29bf2170a6c26ddb9f3ecc9a543235a6ad4
Jabrail Chumakov <67557966+jabrail-chumakov@users.noreply.github.com>
2020-11-13 00:55:46 +0500

    Add files via upload

---
+++ Lab6/train_fk.m
@@ -0,0 +1,36 @@
+data_size = 5000;
+[XTrain, YTrain] = create_dataset_fk(data_size);
+
+XTrain = reshape(XTrain', [1, 1, size(XTrain,2),size(XTrain,1)]);
+
+nFeatures = 5;
+numClasses = 3;
+
+layers = [ ...
+     imageInputLayer([1 1 nFeatures]);
+    fullyConnectedLayer(512)
+    leakyReluLayer
+    fullyConnectedLayer(1024)
+    eluLayer
+    fullyConnectedLayer(1024)
+    reluLayer
+    fullyConnectedLayer(numClasses)  
+    regressionLayer
+    ]
+
+maxEpochs = 200;
+miniBatchSize = 32;
+
+options = trainingOptions('adam', ...
+    'ExecutionEnvironment','cpu', ...
+    'GradientThreshold',1, ...
+    'MaxEpochs',maxEpochs, ...
+    'MiniBatchSize',miniBatchSize, ...
+    'SequenceLength','longest', ...
+    'Shuffle','never', ...
+    'Verbose',0, ...
+    'Plots','training-progress');
+
+net_fk = trainNetwork(XTrain, YTrain,layers,options);
+
+save net_fk
